0082d565a3a609f227353a6cc8bf54df7368bf82
Erick Silva <erick.ap.silva@gmail.com>
2021-08-28 19:51:30 -0300

    Get user by Id and 404 status exception implementation

--- src/main/java/com/ericksilva/workshopmongo/service/UserService.java
+++ src/main/java/com/ericksilva/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.ericksilva.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ericksilva.workshopmongo.domain.User;
 import com.ericksilva.workshopmongo.repository.UserRepository;
+import com.ericksilva.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(()-> new ObjectNotFoundException("Object not found: " + id));
 	}
 }
