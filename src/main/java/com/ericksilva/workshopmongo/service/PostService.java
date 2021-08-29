6a36a43f44a6f1384be4b7341f0fa38596034397
Erick Silva <erick.ap.silva@gmail.com>
2021-08-29 17:01:01 -0300

    Getting Post by ID

---
+++ src/main/java/com/ericksilva/workshopmongo/service/PostService.java
@@ -0,0 +1,23 @@
+package com.ericksilva.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ericksilva.workshopmongo.domain.Post;
+import com.ericksilva.workshopmongo.repository.PostRepository;
+import com.ericksilva.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(()-> new ObjectNotFoundException("Object not found: " + id));
+	}	
+	
+}
