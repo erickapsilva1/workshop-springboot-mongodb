35f14077329e2cb91c7eb740eebd1ff25482756b
Erick Silva <erick.ap.silva@gmail.com>
2021-08-30 07:32:56 -0300

    Simple query with Query Methods

--- src/main/java/com/ericksilva/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/ericksilva/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.ericksilva.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.ericksilva.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
